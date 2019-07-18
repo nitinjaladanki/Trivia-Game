b2bd4a752f5e43fa91face10d8dba3a8398ceba4
Nitin Jaladanki <nitin.jaladanki@gmail.com>
2019-07-18 11:16:28 -0700

    Initial Commit

---
+++ Trivia Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia Game
+//
+//  Created by Chicken Nugget on 2019. 07. 18..
+//  Copyright © 2019. Nitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
